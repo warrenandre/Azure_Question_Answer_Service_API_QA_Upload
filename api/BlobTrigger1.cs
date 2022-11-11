a4e19acb389cfcb3a7c65e096a6c1cd8333fedb7
Warren Joubert <wjoubert@microsoft.com>
2022-11-11 15:14:24 +0200

    updated function with additional phrases

--- api/BlobTrigger1.cs
+++ api/BlobTrigger1.cs
@@ -11,6 +11,9 @@ using Azure.Core;
 using Azure.AI.Language.QuestionAnswering;
 using Azure;
 using Azure.AI.Language.QuestionAnswering.Authoring;
+using System.Text;
+using Newtonsoft.Json;
+using DocumentFormat.OpenXml;
 
 namespace Company.Function
 {
@@ -22,6 +25,9 @@ namespace Company.Function
             log.LogInformation($"C# Blob trigger function Processed blob\n Name:{name} \n Size: {myBlob.Length} Bytes");
             if (name == "QATemplate.xlsx")
             {
+                var projectName = Environment.GetEnvironmentVariable("project_name");
+                var endpointURI = Environment.GetEnvironmentVariable("endpointURI");
+                var azureCreds = Environment.GetEnvironmentVariable("AzureKeyCredential");
                 using (SpreadsheetDocument doc = SpreadsheetDocument.Open(myBlob, false))
                 {
                     WorkbookPart workbookPart = doc.WorkbookPart;
@@ -36,53 +42,80 @@ namespace Company.Function
 
                     log.LogInformation(string.Format("Row count = {0}", rows.LongCount()));
                     log.LogInformation(string.Format("Cell count = {0}", cells.LongCount()));
-                    var qarecords = new List<Record>();
+                    var qarecords = new List<QnaPair>();
+
                     foreach (var row in rows.Skip(1))
                     {
-                        var tquestion = sst.ChildElements[int.Parse(((Cell)row.ChildElements[0]).CellValue.Text)].InnerText;
-                        var record = new Record
+                        var tquestion = sst.ChildElements[int.Parse(((Cell)row.ChildElements[0]).CellValue.Text)]?.InnerText;
+                        var record = new QnaPair
                         {
-                            question = sst.ChildElements[int.Parse(((Cell)row.ChildElements[0]).CellValue.Text)].InnerText,
-                            answer = sst.ChildElements[int.Parse(((Cell)row.ChildElements[1]).CellValue.Text)].InnerText,
-                            country = sst.ChildElements[int.Parse(((Cell)row.ChildElements[2]).CellValue.Text)].InnerText,
-                            city = sst.ChildElements[int.Parse(((Cell)row.ChildElements[3]).CellValue.Text)].InnerText,
-                            code = sst.ChildElements[int.Parse(((Cell)row.ChildElements[4]).CellValue.Text)].InnerText,
-                            additionalPhrases = sst.ChildElements[int.Parse(((Cell)row.ChildElements[4]).CellValue.Text)].ToString().Split(";")
+                            question = sst.ChildElements[int.Parse(((Cell)row.ChildElements[0]).CellValue.Text)]?.InnerText,
+                            answer = sst.ChildElements[int.Parse(((Cell)row.ChildElements[1]).CellValue.Text)]?.InnerText,
+                            country = sst.ChildElements[int.Parse(((Cell)row.ChildElements[2]).CellValue.Text)]?.InnerText,
+                            city = sst.ChildElements[int.Parse(((Cell)row.ChildElements[3]).CellValue.Text)]?.InnerText,
+                            code = sst.ChildElements[int.Parse(((Cell)row.ChildElements[4]).CellValue.Text)]?.InnerText,
+                            additionalLocationPhrases = row.ChildElements[5] != null ? sst.ChildElements[int.Parse(((Cell)row.ChildElements[5]).CellValue.Text)]?.InnerText : ""
+                            
                         };
                         qarecords.Add(record);
                     }
 
                     try
                     {
-                        Uri endpoint = new Uri("");
-                        AzureKeyCredential credential = new AzureKeyCredential("");
+                        Uri endpoint = new Uri(endpointURI);
+                        AzureKeyCredential credential = new AzureKeyCredential(azureCreds);
 
                         QuestionAnsweringAuthoringClient client = new QuestionAnsweringAuthoringClient(endpoint, credential);
+
+                        var operation = "add";
+                        uint rowcounter = 1;
+                        // var additionalPhrasesList = new Dictionary<string, string>();
+                        // var additionalphrasses = qarecords.Where(x => x.additionalLocationPhrases != null);
+                        // foreach (var additionalPhrase in additionalphrasses)
+                        // {
+                        //     additionalPhrasesList.Add(additionalPhrase.code, additionalPhrase.additionalLocationPhrases);
+                        // }
+
                         foreach (var record in qarecords)
                         {
-                            var request = RequestContent.Create(
-                            new[]{
-                            new
-                            {
-                                op="replace",
-                                Value = new
-                                {
-                                    questions=new[]
-                                    {
-                                        $"{record.question}"
-                                    },
-                                    answer=$"{record.answer}",
-                                    metadata=new Dictionary<string,string>{
-                                        {"country",record.country},
-                                        {"city",record.city},
-                                        {"code",record.code}
-                                        }
-                                }
-                            }
-                              });
-                            var result = client.UpdateQnas(WaitUntil.Completed, "annabot", request);
-                        }
 
+                            var rrr = new Dictionary<string, string> { { "test", "test" } };
+                            var metadataa = new string[] { "kzn", "durbs", "amanzimtoti" };
+                            var request = RequestContent.Create(
+                            new[]
+                            {
+                                    new
+                                    {
+                                        op=operation,
+                                        Value = new
+                                        {
+                                            questions=new[]
+                                            {
+                                                $"{record.question}"
+                                            },
+                                            answer=$"{record.answer}",
+                                            metadata=new Dictionary<string, string>
+                                            {
+                                                {"country",record.country},
+                                                {"city",record.city},
+                                                {"code",record.code},
+                                                {"locphrases" ,record.additionalLocationPhrases}
+
+                                            }
+                                        }
+                                    }
+                            });
+
+                            Operation<Pageable<BinaryData>> updateQnasOperation = client.UpdateQnas(WaitUntil.Started, projectName, request);
+
+                            var qnaOpsResult = updateQnasOperation.Value.ToList();
+
+                            var qnaOpsResultString = Encoding.ASCII.GetString(qnaOpsResult.First());
+                            var qnaRoot = JsonConvert.DeserializeObject<Root>(qnaOpsResultString);
+                            qarecords[((int)rowcounter)].id = qnaRoot.id.ToString();
+
+                            rowcounter++;
+                        }
 
                     }
                     catch (Exception ex)
@@ -97,15 +130,50 @@ namespace Company.Function
             }
         }
 
-    }
 
-    public class Record
-    {
-        public string question { get; set; }
-        public string answer { get; set; }
-        public string country { get; set; }
-        public string city { get; set; }
-        public string code { get; set; }
-        public Array additionalPhrases { get; set; }
+
+
+
+        public class QnaPair
+        {
+            public string id { get; set; }
+            public string question { get; set; }
+            public string answer { get; set; }
+            public string country { get; set; }
+            public string city { get; set; }
+            public string code { get; set; }
+            public string additionalLocationPhrases { get; set; }
+        }
+
+        public class Dialog
+        {
+            public bool isContextOnly { get; set; }
+            public List<object> prompts { get; set; }
+        }
+
+        public class Metadata
+        {
+            public string country { get; set; }
+            public string city { get; set; }
+            public string code { get; set; }
+            public string system_metadata_qna_edited_manually { get; set; }
+        }
+
+        public class Root
+        {
+            public int id { get; set; }
+            public string answer { get; set; }
+            public string source { get; set; }
+            public List<string> questions { get; set; }
+            public Metadata metadata { get; set; }
+            public Dialog dialog { get; set; }
+            public List<object> activeLearningSuggestions { get; set; }
+            public bool isDocumentText { get; set; }
+            public string lastUpdatedDateTime { get; set; }
+        }
+
     }
 }
+
+
+
